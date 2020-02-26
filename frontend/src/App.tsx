cc39cb76f73bbe2fae10e44ac8714b2a320c91be
James O'Donoghue <jodonogh1@gmail.com>
2020-02-25 21:42:58 -0800

    implemented basic react frontend that calls a backend /login route with a link

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <a href="http://localhost:5000/auth/google">Login</a>
+        </div>
+    );
+}
+
+export default App;
