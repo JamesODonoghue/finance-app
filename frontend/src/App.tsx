978c61f9db91d06bae560cb1cafffc3895cebbf5
James O'Donoghue <jodonogh1@gmail.com>
2020-03-15 09:19:32 -0700

    added basic router to app, updated some root styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import queryString from 'query-string';
+import { Route } from 'react-router';
+import { Home } from './components/Home';
 
-export const App = (props: any) => {
-    let [token, setToken] = useState(null);
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { Authenticate } from './components/Authenticate';
 
-    useEffect(() => {
-        let query = queryString.parse(props.location.search);
-        if (query.token) {
-            setToken(query.token as any);
-            console.log(token);
-            window.localStorage.setItem('jwt', query.token as string);
-        }
-    });
-
+export const App = () => {
     return (
         <div className="app">
-            {!token ? (
-                <a href="http://localhost:5000/auth/google">Login</a>
-            ) : (
-                <div>You're logged in!</div>
-            )}
+            <div className="main-content">
+                <Route exact path="/" component={Authenticate} />
+                <ProtectedRoute path="/home" component={Home} />
+            </div>
         </div>
     );
 };
