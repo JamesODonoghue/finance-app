81d1957636c64ba80581c027e37cdcfdb5a2a8a6
James O'Donoghue <jodonogh1@gmail.com>
2020-03-19 20:01:59 -0700

    styling updates

--- frontend/src/Home/Accounts/Accounts.tsx
+++ frontend/src/Home/Accounts/Accounts.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Accounts.css';
+import { Activity } from '../Activity/Activity';
+
 export const Accounts = () => {
     return (
         <div className="accounts">
             <div className="accounts_title">Accounts</div>
             <div className="accounts_body">
                 <div className="account">
-                    <div className="account_name">Simple</div>
-                    <div className="account_balance">$3,456</div>
+                    <div className="account_name">Balance</div>
+                    <div className="account_balance">$3,456.00</div>
+                    <div></div>
+                </div>
+                <div className="account">
+                    <div className="account_name">Balance</div>
+                    <div className="account_balance">$3,456.00</div>
+                    <div></div>
+                </div>
+                <div className="account">
+                    <div className="account_name">Balance</div>
+                    <div className="account_balance">$3,456.00</div>
                     <div></div>
                 </div>
             </div>
